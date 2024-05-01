a2ef6d6eb546abae4bf0ff0559c3792b4a493ea8
Viktor Skott <pahomstools@gmail.com>
2024-05-01 12:12:21 +0300

    Задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,7 +2,7 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
-  this.marks = [''];
+  this.marks = [];
 }
 
 let studentFirst = new Student("Виктор", "мужской", 25);
@@ -14,11 +14,19 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
+  if (this.marks) {
+    this.marks.push(...marks);
+  } else {
     this.marks = [...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-    return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+    if (this.marks.length > 0) {
+      return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+    } else {
+      return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
